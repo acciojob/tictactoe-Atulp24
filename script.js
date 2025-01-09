24e7fc51084e60ce0a5a9e240c7a7b661d7ed135
Atulp24 <apk200209@gmail.com>
2025-01-09 19:10:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -43,23 +43,21 @@ document.querySelectorAll(".cell").forEach((cell) => {
           }
         });
       });
-
-      // Check for a winner
       function checkWinner() {
-        const winningCombinations = [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6],
-        ];
-
-        return winningCombinations.some((combination) => {
-          return combination.every((index) => {
-            return gameBoard[index] === (currentPlayer === player1 ? "X" : "O");
-          });
-        });
-      }
+	    const winningCombinations = [
+	      [0, 1, 2],
+	      [3, 4, 5],
+	      [6, 7, 8],
+	      [0, 3, 6],
+	      [1, 4, 7],
+	      [2, 5, 8],
+	      [0, 4, 8],
+	      [2, 4, 6],
+	    ];
+	
+	    return winningCombinations.some((combination) => {
+	      return combination.every((index) => {
+	        return gameBoard[index] === (currentPlayer === player1 ? "X" : "O");
+	      });
+	    });
+}
